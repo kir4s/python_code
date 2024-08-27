05b1a69553287b43b7146b1bd402ac11b2b96d80
kiras <remixsidenation@gmail.com>
2024-08-27 13:01:16 -0300

    codchange

--- aula03.py
+++ aula03.py
@@ -1,4 +1,4 @@
 n1 = int(input('digite um numero; '))
 n2 = int(input('digite outro numero: '))
 s = n1 + n2 
-print('a soma vale:{}'.format(s))
+print('a soma vale entre',n1, 'e', n2,'{}:'.format(s)) 
