efe0c834bc3866040140a34c6162119f0d8d61ab
kiras <remixsidenation@gmail.com>
2024-08-29 15:03:42 -0300

    progresso nas aulas

---
+++ aula06.py
@@ -0,0 +1,7 @@
+n = int(input('ditgite um numero'))
+d = n * 2 
+t = n ** 3
+r = n ** (1/2)
+# \n utiulizado pra descer uma linha (quando mostrado no terminal)
+print('o dobro de {} vale {}.'.format(n, d))
+print('o triplo de {} vale {}. \nA raiz quadrada de {} e igual a {}.'.format(n, t, n, r))
