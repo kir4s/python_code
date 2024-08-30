9b808a281bbd2126ff6657346bbb7c036e9e0283
kiras <remixsidenation@gmail.com>
2024-08-30 00:43:42 -0300

    progresso nas aulas

--- aula10.py
+++ aula10.py
@@ -1,4 +1,13 @@
 real = float(input('Quanto dinheiro você tem na carteira? R$'))
-dolar = real /5.63
+# cotaçao
+cotacao_dolar = 5.63
+cotacao_euro = 6.17
+# Cálculo
+dolar = real / cotacao_dolar
+euro = real / cotacao_euro
+#saida
 print('com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
-# comando ":.2f" pra ter duas casas decimais flutuantes 
+print('com R${:.2f} voce pode comprar EUR{:.2f}'.format(real, euro))
+
+# comando ":.2f" pra ter duas casas decimais flutuantes
+
