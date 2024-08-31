bcdae41d5e64731ad03a40797c43894dedfe2a3c
kiras <remixsidenation@gmail.com>
2024-08-30 23:02:02 -0300

    exerc parecido com o anterior

---
+++ aula13.py
@@ -0,0 +1,3 @@
+sala = float(input('digite seu salario para saber quanto fica com aumento de 15%:R$ '))
+aumt = sala + (sala * 15 / 100)
+print('o seu salario com aumento de 15% é equivalente a {:.2f}'.format(aumt))
