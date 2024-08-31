db243687641a0910af1638a9ed632427ba0ff334
kiras <remixsidenation@gmail.com>
2024-08-31 04:11:07 -0300

    finalizaçâo da aula

--- aulateo08.py
+++ aulateo08.py
@@ -6,3 +6,4 @@ print('a raiz de {} é igual a {:.2f}'.format(num, raiz))
 # Bom como observaçâo dessa aula, temos duas formas de importar bibliotecas, uma elas vc importa a biblioteca completa
 # e pode usar toda suas funçoes ex:import math(usando esse comando podemos usar todas as funçoes de matematica da biblioteca)
 # a outra forma de importar funçoes de uma biblioteca é puxando a funçao especifica ex:from math import sqrt floor 
+# o import pode ser usado pra qualquer biblio e qualquer uma das duas funçoes 
