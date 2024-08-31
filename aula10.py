31bb5ddca896dc888f880d0fe690ffedfab972b0
kiras <remixsidenation@gmail.com>
2024-08-30 22:09:53 -0300

    novos exerc

--- aula10.py
+++ aula10.py
@@ -8,6 +8,4 @@ euro = real / cotacao_euro
 #saida
 print('com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
 print('com R${:.2f} voce pode comprar EUR{:.2f}'.format(real, euro))
-
 # comando ":.2f" pra ter duas casas decimais flutuantes
-
