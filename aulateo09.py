aacd19b53670549bf47086a2a73da0198963f903
kiras <remixsidenation@gmail.com>
2024-09-03 01:17:08 -0300

    aula teorica finalizada

---
+++ aulateo09.py
@@ -0,0 +1,10 @@
+frase = 'Curso em Video Python'
+print(frase.replace('Python', 'android'))
+
+
+
+
+
+
+
+# obs da aula: Fatiamento de String, Análise com len(), count(), find(), transformações com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().
