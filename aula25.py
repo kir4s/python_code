4daab1750e32e14d0ab4554a999fe7d4276c3e27
kiras <remixsidenation@gmail.com>
2024-09-04 16:58:46 -0300

    exerc

---
+++ aula25.py
@@ -0,0 +1,6 @@
+nome = str(input('Qual seu nome completo? ')).strip()
+print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
+
+
+
+#exerc parecido com o passado mas usando uma nova posibilidade (in) IN É UM OPERADOR 
