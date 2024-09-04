cf7d8400705ffe18dccaffb8d5cc965fadad65ad
kiras <remixsidenation@gmail.com>
2024-09-04 16:50:05 -0300

    exerc

---
+++ aula24.py
@@ -0,0 +1,9 @@
+cid = str(input('Em que cidade você nasceu? ')).strip()
+print(cid[:5].upper() == 'SANTO')
+
+
+
+
+#strip usada pra corrigir erros de digitaçao 
+#uper pra faciltar na confirmaçao da palavra na str 
+#codigo pra confirmar se uma palavra ta escrita dentro de uma variavel 
