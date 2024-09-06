725ccb93ac8bf8abbce2ffaf33e16bd79316df43
kiras <remixsidenation@gmail.com>
2024-09-06 19:32:08 -0300

    aula teorica

---
+++ aulateo10.py
@@ -0,0 +1,17 @@
+tempo = int(input('Quantos anos tem seu carro?'))
+if tempo <=3:
+    print('Carro novo')
+
+else:
+    #print('Carro velho')
+    print('--FIM--')    
+
+
+
+
+#aula sobre if else
+#exemplo do mesmo programa com uma condiçao simplificada
+tempo=int(input('Quantos anos tem seu carro?'))
+print('carro novo'if tempo<=3 else 'carro velho')
+print('--FIM--')
+
