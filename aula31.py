15c38fd2eae45cc15b4d433a8677f6c24f376e5b
kiras <remixsidenation@gmail.com>
2024-09-11 19:50:48 -0300

    .

--- aula31.py
+++ aula31.py
@@ -1,7 +1,7 @@
 km = int(input('Digite quantos km será sua viagem: '))
 if km <= 200:
     a = km * 0.50 
-    print('Sua viagem custará R${} '.format(a))
+    print('Sua viagem custará R${} .'.format(a))
 else:
     a = km * 0.45
-    print('Sua viagem custará R${} '.format(a))
+    print('Sua viagem custará R${} .'.format(a))
