43f6e40ce5ca9b275caafb23360f6901064376bf
kiras <remixsidenation@gmail.com>
2024-09-12 23:29:25 -0300

    correçâo do codigo

--- aula28.py
+++ aula28.py
@@ -1,12 +1,13 @@
-import random 
-
-
-print('Bem vindo, nesse mini game irei pensar em um numero de 1-5, boa sorte pra acertar')
-numero_aleatorio = random.randint(1,5)
-a = int(input('digite um numero: '))
-if a == numero_aleatorio:
-    print('Parabens você acertou!')
+from random import randint
+from time import sleep #funçao sleep faz o pc dormir e ter um atraso na linha de codigo
+computador = randint(0, 5)
+print('--' * 20)
+print('Vou pensar em um numero entre 0-5. Tente adivinhar...')
+print('--' * 20)
+jogador = int(input('Em que número eu pensei?: '))
+print('PROCESSANDO...')
+sleep(3)
+if jogador == computador:
+    print('PARABENS! Você conseguiu me vencer!')
 else:
-    print('você errou!')
-    print('--GAME-OVER--')
-
+    print('Eu pensei no numero {} nâo no numero {}!'.format(computador, jogador))
