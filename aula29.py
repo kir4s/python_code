13716bdae2b13d738c2628cd24f0bff432903275
kiras <remixsidenation@gmail.com>
2024-09-16 17:24:13 -0300

    progresso nas aulas

--- aula29.py
+++ aula29.py
@@ -1,7 +1,7 @@
 print('Bem vindo ao detran')
 
 
-km = int(input('Quantos km você estava na hora da multa?: '))
+km = int(input('Quantos km você estava na hora da multa?: ')) 
 if km <= 80:
     print('Essa multa foi um erro, você esta liberado!')
 else:
